33b8375d1d1465483055774d00962765d7d97fc1
AlinaCalistru <73351416+AlinaCalistru@users.noreply.github.com>
2022-03-28 17:56:46 +0100

    new functions

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-// witch hero object
-const hero ={
-    elementId:"hero",
-    name:"Witch",
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
     avatar: "images/witch1.jpg",
     health: 60,
-    diceRoll: [3,1, 4],
-    diceCount: 2
+    diceRoll: [3, 1, 4],
+    diceCount: 3
 }
 
-
-// wizard monster object
-const monster ={
-    elementId:"monster",
-    name:"Wizard",
+const monster = {
+    elementId: "monster",
+    name: "Orc",
     avatar: "images/wizard.jpg",
     health: 10,
-    diceRoll: [4],
-    diceCount: 2
+    diceRoll: [6],
+    diceCount: 1
+}
+
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(function(){
+        return Math.floor(Math.random() * 6) + 1
+    })
+//     let newDiceRolls = [];
+//     for (let i = 0; i < diceCount; i++) {
+//         newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+//     }
+//    return newDiceRolls;
 }
 
 
-function renderCharacter(data){
-    const {elementId, name, avatar, health, diceRoll, diceCount} = data;
-    let diceHtml = "";
+function getDiceHtml(diceCount){
+    return getDiceRollArray(diceCount).map(function(num){
+        return `<div class="dice">${num}</div>`
+    }).join(" ");
+}
 
-    for(let i = 0; i < diceCount; i ++){
-        diceHtml += `<div class="dice">6</div>`
-    }
-   
-    document.getElementById(elementId).innerHTML=
-    `<div class="character-card">
-    <h4 class="name"> ${name} </h4>
-    <img class="avatar" src="${avatar}"/>
-    <p class="health">health: <b> ${health} </b></p>
-    <div class="dice-container"> ${diceHtml}</div>
-</div>`;   
-  
+
+
+
+function renderCharacter(data) {
+    const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+    const diceHtml = getDiceHtml(diceCount)
+
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">    
+                ${diceHtml}
+            </div>
+        </div>`;
 }
 
 renderCharacter(hero);
 renderCharacter(monster);
-
-
-
